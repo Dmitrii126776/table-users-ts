6c9bf74328995a03b2f0b399222cebe1057d709f
Dmitrii126776 <dukek67@gmail.com>
2023-07-03 21:52:02 -0400

    Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div style={{paddingTop: '60px'}}>
-            <h1>Home</h1>
+            <h1>Home Page</h1>
         </div>
     );
 };
